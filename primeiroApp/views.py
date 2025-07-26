57d04987c07f312477b0db6ac5f336b507d61cc9
Bruno Cesar Machado da Silva <59833300+BrunoCesarMS@users.noreply.github.com>
2025-07-26 19:33:56 +0000

    Etapa 2 imersao

---
+++ primeiroApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def atracoes(request):
+    return render(request, 'atracoes.html')
+
+def galeria(request):
+    return render(request, 'galeria.html')
